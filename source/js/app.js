815ac8660e4348fd9cea8a7dd5d5255a070fdbc6
podyankee <podyankee@gmail.com>
2018-11-03 07:35:06 +0300

    banner bottom added

--- source/js/app.js
+++ source/js/app.js
@@ -23,6 +23,41 @@ $(document).ready(function(){
 			]
 		});
 	};
+	let featuredSlider = () => {
+		$('.js-featured').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			dots: false,
+			prevArrow: ".featured__navigation--prev",
+			nextArrow: ".featured__navigation--next",
+			mobileFirst: true,
+			responsive: [
+				{
+					breakpoint: 0,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						arrows: false
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1
+					}
+				},
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1
+					}
+				},
+			]
+		});
+	};
 
 		bannerSlider();
+		featuredSlider();
     });
